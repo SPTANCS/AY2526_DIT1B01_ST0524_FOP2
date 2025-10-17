2a014230ceed7a8920cc2aabb3ef06414b42ec54
Tan CS <tan_chee_seong@sp.edu.sg>
2025-10-17 11:03:29 +0800

    update example01 to receive user input

--- tutorial/01_Advanced_JavaScript_Part_1/example01.js
+++ tutorial/01_Advanced_JavaScript_Part_1/example01.js
@@ -1,4 +1,9 @@
+const input = require("readline-sync");
+
 console.clear();
 
 console.log("All is good");
 
+let name = input.question("What is your name ? ");
+
+console.log(`Welcome to ST0524 FOP2 ${name} :)`);
