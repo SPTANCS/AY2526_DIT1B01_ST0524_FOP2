6800f0521afdcbf5f73874b6dcb3375d1695c0b1
Tan CS <tan_chee_seong@sp.edu.sg>
2025-10-23 10:47:31 +0800

    update example02

---
+++ tutorial/01_Advanced_JavaScript_Part_1/example02.js
@@ -0,0 +1,29 @@
+console.clear();
+
+const input = require('readline-sync');
+
+console.log('Welcome to the number guesssing game');
+
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// console.log(`randomNumber is ${randomNumber}`);
+
+var counter = 0;
+
+do {
+
+  var userGuess    = parseInt(input.question("Enter your guess >> "));
+  counter++;
+
+  // console.log(typeof userGuess);
+  // console.log(`userGuess is ${userGuess}`);
+
+  if (userGuess < randomNumber) {
+    console.log('Your guess is too low.');
+  } else if (userGuess > randomNumber) {
+    console.log('Your guess is too high.');
+  } else {
+    console.log(`You are correct, the number is ${randomNumber}, you too ${counter} turn(s)`);
+  }
+
+} while (randomNumber != userGuess ); // repeat while the guess is wrong
