3a006146d7428bb58557f9b58f1048b0d8cde74b
Tan CS <tan_chee_seong@sp.edu.sg>
2025-11-13 11:24:25 +0800

    reduce example

--- tutorial/04_JavaScript_Data_Structures_Part01/example12_reduceObj.js
+++ tutorial/04_JavaScript_Data_Structures_Part01/example12_reduceObj.js
@@ -14,9 +14,56 @@ const cars = [
 console.clear();
 console.log(cars);
 
-let totalPrice = ______;
-
+let totalPrice = cars.reduce( (currTotal, car ) => {
+  return currTotal + car.price;
+}, 0);
 
 let averagePrice = totalPrice / cars.length;
 
 console.log(`Average car price is SGD ${averagePrice}`);
+
+let mostExpensiveCar = cars.reduce( (currMostExpensive, currCar) => {
+  if (currCar.price > currMostExpensive.price) {
+    return currCar;
+  } else {
+    return currMostExpensive;
+  }
+});
+
+console.log(`Most Expensive car is ${mostExpensiveCar.brandName}: ${mostExpensiveCar.modelNumber} price is SGD ${mostExpensiveCar.price}`);
+
+let cheapestCar = cars.reduce( (currCheapest, currCar) => {
+  if (currCar.price < currCheapest.price) {
+    return currCar;
+  } else {
+    return currCheapest;
+  }
+});
+
+console.log(`Cheapest car is ${cheapestCar.brandName}: ${cheapestCar.modelNumber} price is SGD ${cheapestCar.price}`);
+
+// another way
+// sort the car array
+// cars.sort((carA, carB) => {
+//   if (carA.price > carB.price) { // price of car A is higher
+//     return 1;
+//   } else if (carA.price < carB.price) {
+//     return -1;
+//   } else {
+//     return 0;
+//   }
+// });
+
+cars.sort((carA, carB) => carA.price - carB.price);
+
+console.log(cars);
+
+const highestPriceCar = cars[cars.length - 1];
+const lowestPriceCar  = cars[0];
+
+console.log();
+console.log(`Most Expensive car is ${highestPriceCar.brandName}: ${highestPriceCar.modelNumber} price is SGD ${highestPriceCar.price}`);
+console.log(`Cheapest car is ${lowestPriceCar.brandName}: ${lowestPriceCar.modelNumber} price is SGD ${lowestPriceCar.price}`);
+
+
+
